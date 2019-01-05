000f2b13a8f1c8e71003718cabbe44dd86fca7b6
cinder <meikopolis@gmail.com>
2019-01-04 20:30:10 -0700

    changed settings

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$+(yd^yshcf=^qs-u^cfn@su@=yl$$uu49s956j(f(xsag2kan'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com',
                  'cinder.pythonanywhere.com']
